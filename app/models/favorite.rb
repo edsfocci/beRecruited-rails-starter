4f1a31187af4290e07b4238507cb7560eefe9aeb
Ed Solis <edsfocci@gmail.com>
2013-10-17 18:07:11 -0700

    Minor edge-case bug fixed- User ordering

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -53,9 +53,17 @@ class Favorite < ActiveRecord::Base
     if array.size == 0
       return dup_array << user_hash
     elsif user_hash[:current_amount] <= array.last[:current_amount]
-      return dup_array << user_hash
+      if self.needs_to_swap?(user_hash, array.last, :up)
+        return dup_array.insert(dup_array.size - 1, user_hash)
+      else
+        return dup_array << user_hash
+      end
     elsif user_hash[:current_amount] >= array.first[:current_amount]
-      return dup_array.unshift(user_hash)
+      if self.needs_to_swap?(user_hash, array.first, :down)
+        return dup_array.insert(1, user_hash)
+      else
+        return dup_array.unshift(user_hash)
+      end
     end
 
     top, middle, bottom = 0, array.size / 2, array.size - 1
@@ -74,16 +82,39 @@ class Favorite < ActiveRecord::Base
     # Edge case: Users with the same current_amount:
       # put in order of last_week_amount
       # Note: designed for this coding challenge only (only 1 with same amount)
-    if user_hash[:current_amount] == array[middle - 1][:current_amount]
-      if user_hash[:last_week_amount] > array[middle - 1][:last_week_amount]
-        middle -= 1
-      end
-    elsif user_hash[:current_amount] == array[middle][:current_amount]
-      if user_hash[:last_week_amount] < array[middle][:last_week_amount]
-        middle += 1
-      end
+    # if user_hash[:current_amount] == array[middle - 1][:current_amount]
+    #   if user_hash[:last_week_amount] > array[middle - 1][:last_week_amount]
+    #     middle -= 1
+    #   end
+    # elsif user_hash[:current_amount] == array[middle][:current_amount]
+    #   if user_hash[:last_week_amount] < array[middle][:last_week_amount]
+    #     middle += 1
+    #   end
+    # end
+
+    if self.needs_to_swap?(user_hash, array[middle - 1], :up)
+      middle -= 1
+    elsif self.needs_to_swap?(user_hash, array[middle], :down)
+      middle += 1
     end
 
     dup_array.insert(middle, user_hash)
+  end
+
+
+  # Edge case: Users with the same current_amount:
+    # put in order of last_week_amount
+    # Note: designed for this coding challenge only (only 1 with same amount)
+  def self.needs_to_swap?(hash1, hash2, direction)
+    if hash1[:current_amount] == hash2[:current_amount]
+      if direction == :up &&
+                            hash1[:last_week_amount] > hash2[:last_week_amount]
+        return true
+      elsif hash1[:last_week_amount] < hash2[:last_week_amount]
+        return true
+      end
+    end
+
+    false
   end
 end
